d50b0044436d86b6a4f10418670227aef4d50def
Student <example@example.com>
2020-01-17 10:31:43 -0800

    create basic html and css styleing for form result blocks

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,36 @@
 $(document).ready(function()  {
   $("form#language_sorter").submit(function(event)  {
+    
+    $("#swift, #c_plus_plus, #python, #ruby").hide();
+
     var answer_1 = $("select#application_type").val();
     var answer_2 = $("select#operating_system").val();
     var answer_3 = $("select#favorite_saga").val();
     var answer_4 = $("select#hours_sleep").val();
-    var answer_5 =$("select#crayon").val();
+    var answer_5 = $("select#crayon").val();
+    event.preventDefault();
 
-    if (answer_1 === "0" || answer_2 === "0" || answer_3 === "0" || answer_4 === "0" || answer_5 === "0") {
-      alert("Please answer all the questions");
-    } else if {
-      
-    }
-
-
-
-
-
-
-    // if (answer_1 === "1") {
-    //   alert("You should learn Swift!");
-    // } else if ( answer_1 === "2") {
-    //   alert("You should learn C++!");
-    // } else if (answer_1 === "3") {
-    //   alert("You should learn Python!");
+    // if (answer_1 === "0" || answer_2 === "0" || answer_3 === "0" || answer_4 === "0" || answer_5 === "0") {
+    //   alert("Please answer all the questions");
     // } else {
-    //   alert("Please answer question 1");
+      
     // }
+
+
+
+
+
+
+    if (answer_1 === "1") {
+      $("#swift").show();
+    } else if ( answer_1 === "2") {
+      $("#c_plus_plus").show();
+    } else if (answer_1 === "3") {
+      $("#python").show();
+      $("#ruby").show();
+    } else {
+      alert("Please answer question 1");
+    }
 
     // if (answer_2 === "1") {
     //   alert("You should learn Swift or Python!")
