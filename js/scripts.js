dd4a36e3d736635544c30bd0ed604c734700bc9d
Student <example@example.com>
2020-01-17 09:42:58 -0800

    add 2 more forms in index.html with jquery logic in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,35 @@
 $(document).ready(function()  {
   $("form#language_sorter").submit(function(event)  {
-    var answer = $("select#application_type").val();
-    alert(answer);
-    if (answer === "1") {
-      alert("That's a good answer!");
+    var answer_1 = $("select#application_type").val();
+    var answer_2 = $("select#operating_system").val();
+    var answer_3 = $("select#favorite_saga").val();
+
+    // if (answer_1 === "1") {
+    //   alert("You should learn Swift!");
+    // } else if ( answer_1 === "2") {
+    //   alert("You should learn C++!");
+    // } else if (answer_1 === "3") {
+    //   alert("You should learn Python!");
+    // } else {
+    //   alert("Please answer question 1");
+    // }
+
+    // if (answer_2 === "1") {
+    //   alert("You should learn Swift or Python!")
+    // } else if (answer_2 === "2") {
+    //   alert("You should learn C++ or Python!")
+    // } else {
+    //   alert("Please answer question 2:")
+    // }
+
+    if (answer_3 === "1") {
+      alert("You should learn Swift!");
+    } else if ( answer_3 === "2") {
+      alert("You should learn C++!");
+    } else if (answer_3 === "3") {
+      alert("You should learn Python!");
     } else {
-      alert("That's not a good answer");
+      alert("Please answer question 3");
     }
   });
 });
