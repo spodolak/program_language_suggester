faa84f4e8a4a1da8973780489ebd011ab4927d0a
Student <example@example.com>
2020-01-17 09:10:31 -0800

    troubleshoot jquery form logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function()  {
   $("form#language_sorter").submit(function(event)  {
-    alert("jquery is linked!");
+    var answer = $("select#application_type").val();
+    alert(answer);
+    if (answer === "1") {
+      alert("That's a good answer!");
+    } else {
+      alert("That's not a good answer");
+    }
   });
 });
